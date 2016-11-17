df42fb7de42090b1a0b8e673625ee6015b6d37f9
Nathan Kuczun <nqkuczun01@bohe27db3x.admin.bvsd.k12.co.us>
2016-11-17 13:55:06 -0700

    Added labels to viewcontrollers

---
+++ BHS App IOS/BHS App IOS/ThirdViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ThirdViewController.swift
+//  BHS App IOS
+//
+//  Created by Nathan Kuczun on 11/17/16.
+//  Copyright © 2016 Nathan Kuczun. All rights reserved.
+//
+
+import UIKit
+
+class ThirdViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
